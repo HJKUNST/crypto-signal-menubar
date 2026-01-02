121cead4901251fe135a28415557610d6eab9390
Laura HJ Kim <heejookim1@naver.com>
2026-01-02 11:52:06 +0900

    Initial Commit

---
+++ new-menubar-app/new_menubar_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  new_menubar_appApp.swift
+//  new-menubar-app
+//
+//  Created by 김희주 on 1/2/26.
+//
+
+import SwiftUI
+
+@main
+struct new_menubar_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
